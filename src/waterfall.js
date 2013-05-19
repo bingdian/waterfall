299bce09c8055ebb9d258db69366acd73e8f24c2
bingdian <imbingdian@gmail.com>
2013-05-19 20:48:51 +0800

    modify loading

--- src/waterfall.js
+++ src/waterfall.js
@@ -38,7 +38,7 @@
             
             
             loading: {
-                loadingMsg: 'loading...',
+                loadingMsg: '<div style="text-align:center;"><img src="data:image/gif;base64,R0lGODlhEAALAPQAAP///zMzM+Li4tra2u7u7jk5OTMzM1hYWJubm4CAgMjIyE9PT29vb6KiooODg8vLy1JSUjc3N3Jycuvr6+Dg4Pb29mBgYOPj4/X19cXFxbOzs9XV1fHx8TMzMzMzMzMzMyH5BAkLAAAAIf4aQ3JlYXRlZCB3aXRoIGFqYXhsb2FkLmluZm8AIf8LTkVUU0NBUEUyLjADAQAAACwAAAAAEAALAAAFLSAgjmRpnqSgCuLKAq5AEIM4zDVw03ve27ifDgfkEYe04kDIDC5zrtYKRa2WQgAh+QQJCwAAACwAAAAAEAALAAAFJGBhGAVgnqhpHIeRvsDawqns0qeN5+y967tYLyicBYE7EYkYAgAh+QQJCwAAACwAAAAAEAALAAAFNiAgjothLOOIJAkiGgxjpGKiKMkbz7SN6zIawJcDwIK9W/HISxGBzdHTuBNOmcJVCyoUlk7CEAAh+QQJCwAAACwAAAAAEAALAAAFNSAgjqQIRRFUAo3jNGIkSdHqPI8Tz3V55zuaDacDyIQ+YrBH+hWPzJFzOQQaeavWi7oqnVIhACH5BAkLAAAALAAAAAAQAAsAAAUyICCOZGme1rJY5kRRk7hI0mJSVUXJtF3iOl7tltsBZsNfUegjAY3I5sgFY55KqdX1GgIAIfkECQsAAAAsAAAAABAACwAABTcgII5kaZ4kcV2EqLJipmnZhWGXaOOitm2aXQ4g7P2Ct2ER4AMul00kj5g0Al8tADY2y6C+4FIIACH5BAkLAAAALAAAAAAQAAsAAAUvICCOZGme5ERRk6iy7qpyHCVStA3gNa/7txxwlwv2isSacYUc+l4tADQGQ1mvpBAAIfkECQsAAAAsAAAAABAACwAABS8gII5kaZ7kRFGTqLLuqnIcJVK0DeA1r/u3HHCXC/aKxJpxhRz6Xi0ANAZDWa+kEAA7" alt=""><br />Loading...</div>',
                 ajaxFailedMsg: 'ajax request failed, please try again later',
                 start: undefined,
                 finished: undefined
@@ -55,7 +55,6 @@
             path: undefined, // 瀑布流数据分页url，可以是数组如["/page/", "/"]，或者是根据分页返回一个url方法如：function(page) { return '/populr/' + page; }
             dataType: 'json', //json, jsonp, html
             params: {}, //瀑布流数据请求参数
-            //tpl: $('#waterfall-tpl').html(),
             renderData: function(data) {
                 var tpl = $('#waterfall-tpl').html(),
                     template = Handlebars.compile(tpl);
@@ -238,7 +237,6 @@
                 animationOptions = options.animationOptions,
                 colWidth = options.colWidth,
                 gutterWidth = options.gutterWidth,
-                gutterHeight = options.gutterHeight,
                 len = this.colHeightArray.length,
                 align = options.align,
                 fixMarginLeft,
